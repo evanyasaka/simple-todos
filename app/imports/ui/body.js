347201107e72bae46e877a7bf2be01520e847f90
evanyasaka <eyasaka@hawaii.edu>
2017-02-27 20:51:12 -1000

    Task 3: Collections, Forms and events, update and remove

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -1,23 +1,50 @@
-
-
 import { Template } from 'meteor/templating';
 
+import { Tasks } from '../api/tasks.js';
 
-
+import './task.js';
 import './body.html';
-
-
 
 Template.body.helpers({
 
-  tasks: [
+  tasks() {
 
-    { text: 'This is task 1' },
+    // Show newest tasks at the top
 
-    { text: 'This is task 2' },
+    return Tasks.find({}, { sort: { createdAt: -1 } });
 
-    { text: 'This is task 3' },
+  },
 
-  ],
+});
+
+Template.body.events({
+
+  'submit .new-task'(event) {
+
+    // Prevent default browser form submit
+
+    event.preventDefault();
+
+    // Get value from form element
+
+    const target = event.target;
+
+    const text = target.text.value;
+
+    // Insert a task into the collection
+
+    Tasks.insert({
+
+      text,
+
+      createdAt: new Date(), // current time
+
+    });
+
+    // Clear form
+
+    target.text.value = '';
+
+  },
 
 });
